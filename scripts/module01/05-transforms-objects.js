b224828cc4cf6cb1010c20cca3791c6a652eb607
João Alves <njoao97710@gmail.com>
2021-03-05 01:59:32 -0300

    :sparkles: finish lesson 05

--- scripts/module01/05-transforms-objects.js
+++ scripts/module01/05-transforms-objects.js
@@ -4,24 +4,67 @@ import * as THREE from "three";
 const scene = new THREE.Scene();
 
 const sizes = {
-  width: 1920,
-  height: 945,
+  width: 930,
+  height: 600
 };
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 5;
+// camera.position.set(1, 1, 3)
+camera.position.z = 3;
 
 scene.add(camera);
 
 // Object
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: "red" });
+const material = new THREE.MeshBasicMaterial({ color: "lightblue" });
 const mesh = new THREE.Mesh(geometry, material);
-mesh.position.x = 0.7;
-mesh.position.y = -0.5;
-mesh.position.z = 2;
 
-scene.add(mesh);
+// Positions
+// mesh.position.x = 0.7;
+// mesh.position.y = -0.6;
+// mesh.position.z = 3;
+mesh.position.set(0.7, -0.6, 1);
+// mesh.position.normalize()
+
+// Scale
+// mesh.scale.x = 0.7;
+// mesh.scale.y = -0.6;
+// mesh.scale.z = 3;
+mesh.scale.set(1, 1, 1);
+
+// Rotations
+// mesh.rotation.x = 0.7;
+// mesh.rotation.y = -0.6;
+// mesh.rotation.z = 3;
+mesh.rotation.reorder("YXZ");
+mesh.rotation.set(1, 1, 1);
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "purple" })
+);
+cube2.position.set(0.8, 0.55, 0);
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "lightgreen" })
+);
+
+// Group
+const group = new THREE.Group();
+group.add(mesh, cube2, cube3);
+// group.rotation.y = 2
+// group.scale.y = 2
+scene.add(group);
+
+// Metodo para focar em alguma posição ou objeto
+camera.lookAt(mesh.position);
+
+// Vector3 -> Class para posicionamento no axis
+
+// Axes Helper
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper);
 
 // Lights
 const light = new THREE.AmbientLight(0x404040);
@@ -29,7 +72,7 @@ scene.add(light);
 
 // Render
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.getElementById("canvas"),
+  canvas: document.getElementById("canvas")
 });
 
 renderer.setSize(sizes.width, sizes.height);
