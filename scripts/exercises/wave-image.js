8212c9e5fa4f0f995789d3740898b88d2f0739f1
João Alves <njoao97710@gmail.com>
2021-03-20 15:49:18 -0300

    :sparkles: FEAT: add mesh with simplex noise algorithm for andulation

--- scripts/exercises/wave-image.js
+++ scripts/exercises/wave-image.js
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import OrbitControls from "three/examples/jsm/controls/OrbitControls";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 import vertexShader from "./glsl/vertex.glsl";
 import fragmentShader from "./glsl/fragment.glsl";
@@ -10,15 +10,16 @@ const scene = new THREE.Scene();
 
 const sizes = {
   width: window.innerWidth,
-  height: window.innerHeight,
+  height: window.innerHeight
 };
 
 const fov = 75;
 const aspectRatio = sizes.width / sizes.height;
-const near = 1;
+const near = 0.1;
 const far = 1000;
 
 const camera = new THREE.PerspectiveCamera(fov, aspectRatio, near, far);
+camera.position.z = 1;
 
 scene.add(camera);
 
@@ -28,10 +29,11 @@ controls.enableDamping = true;
 const geometry = new THREE.PlaneGeometry(0.4, 0.6, 16, 16);
 const material = new THREE.ShaderMaterial({
   uniforms: {
-    uTime: { value: 0.0 },
+    uTime: { value: 0.0 }
   },
+  wireframe: true,
   vertexShader,
-  fragmentShader,
+  fragmentShader
 });
 
 const mesh = new THREE.Mesh(geometry, material);
@@ -40,6 +42,33 @@ scene.add(mesh);
 
 const renderer = new THREE.WebGLRenderer({
   canvas: canvasElement,
+  antialias: true
 });
+
 renderer.setSize(sizes.width, sizes.height);
-renderer.render(scene, camera);
+renderer.setClearColor(0xffffff, 1);
+// renderer.render(scene, camera);
+
+const clock = new THREE.Clock();
+
+function tick() {
+  // const currentTime = Date.now();
+  // const deltaTime = currentTime - time;
+  // time = currentTime;
+  // const elapsedTime = clock.getElapsedTime();
+
+  // // Update camera with event mousemove
+  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) + 3;
+  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) + 3;
+  // camera.position.y = -cursor.y * 3;
+  // camera.lookAt(mesh.position);
+  material.uniforms.uTime.value = clock.getElapsedTime();
+
+  // Update controls
+  controls.update();
+
+  // Render in each frame
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+}
+tick();
